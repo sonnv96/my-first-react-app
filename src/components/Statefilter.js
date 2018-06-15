dfc12786c8767512c37eead757cf0de7a795b475
Son <sonnv.vnu@gmail.com>
2018-06-15 17:48:37 +0700

    fix name folder

--- src/components/Statefilter.js
+++ src/components/Statefilter.js
@@ -1,16 +1,16 @@
 import React, {Component, PropTypes} from 'react'
-import search from './search'
+import search from './Search'
 
-class StateFilter extends Component {
+class Statefilter extends Component {
     render() {
         return (
             <div>
                 Show All :<input type='radio' name='test' onChange={() => this.props.filterStatus(1)}/>
-                Active : <input type='radio' name='test' onChange={() => this.props.filterStatus(2)}/>
+                Active   : <input type='radio' name='test' onChange={() => this.props.filterStatus(2)}/>
                 UnActive : <input type='radio' name='test' onChange={() => this.props.filterStatus(3)}/>
             </div>
         )
     }
 }
 
-export default StateFilter
+export default Statefilter
